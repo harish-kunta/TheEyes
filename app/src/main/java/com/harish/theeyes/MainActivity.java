a35e6434487d9c3fd1a8cade1412c1ba4d48b261
HarishKunta <33089746+harishkunta@users.noreply.github.com>
2021-02-07 23:47:18 -0500

    latest commit

--- app/src/main/java/com/harish/theeyes/MainActivity.java
+++ app/src/main/java/com/harish/theeyes/MainActivity.java
@@ -15,28 +15,59 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Matrix;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.util.Rational;
 import android.util.Size;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okio.Buffer;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Header;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 
-//saus: https://codelabs.developers.google.com/codelabs/camerax-getting-started/
+import android.preference.PreferenceManager;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private int REQUEST_CODE_PERMISSIONS = 10; //arbitrary number, can be changed accordingly
-    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA","android.permission.WRITE_EXTERNAL_STORAGE"}; //array w/ permissions from manifest
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"}; //array w/ permissions from manifest
     TextureView txView;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +76,9 @@ public class MainActivity extends AppCompatActivity {
 
         txView = findViewById(R.id.view_finder);
 
-        if(allPermissionsGranted()){
+        if (allPermissionsGranted()) {
             startCamera(); //start camera if permission has been granted by user
-        } else{
+        } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
     }
@@ -59,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         /* start preview */
         int aspRatioW = txView.getWidth(); //get width of screen
         int aspRatioH = txView.getHeight(); //get height
-        Rational asp = new Rational (aspRatioW, aspRatioH); //aspect ratio
+        Rational asp = new Rational(aspRatioW, aspRatioH); //aspect ratio
         Size screen = new Size(aspRatioW, aspRatioH); //size of the screen
 
         //config obj for preview/viewfinder thingy.
@@ -70,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
                 new Preview.OnPreviewOutputUpdateListener() {
                     //to update the surface texture we have to destroy it first, then re-add it
                     @Override
-                    public void onUpdated(Preview.PreviewOutput output){
+                    public void onUpdated(Preview.PreviewOutput output) {
                         ViewGroup parent = (ViewGroup) txView.getParent();
                         parent.removeView(txView);
                         parent.addView(txView, 0);
@@ -94,15 +125,17 @@ public class MainActivity extends AppCompatActivity {
                 imgCap.takePicture(file, new ImageCapture.OnImageSavedListener() {
                     @Override
                     public void onImageSaved(@NonNull File file) {
+
                         String msg = "Photo capture succeeded: " + file.getAbsolutePath();
-                        Toast.makeText(getBaseContext(), msg,Toast.LENGTH_LONG).show();
+                        uploadToServer(file);
+                        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
                     }
 
                     @Override
                     public void onError(@NonNull ImageCapture.UseCaseError useCaseError, @NonNull String message, @Nullable Throwable cause) {
                         String msg = "Photo capture failed: " + message;
-                        Toast.makeText(getBaseContext(), msg,Toast.LENGTH_LONG).show();
-                        if(cause != null){
+                        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+                        if (cause != null) {
                             cause.printStackTrace();
                         }
                     }
@@ -116,18 +149,113 @@ public class MainActivity extends AppCompatActivity {
         ImageAnalysis analysis = new ImageAnalysis(imgAConfig);
 
         analysis.setAnalyzer(
-                new ImageAnalysis.Analyzer(){
+                new ImageAnalysis.Analyzer() {
                     @Override
-                    public void analyze(ImageProxy image, int rotationDegrees){
+                    public void analyze(ImageProxy image, int rotationDegrees) {
                         //y'all can add code to analyse stuff here idek go wild.
                     }
                 });
 
         //bind to lifecycle:
-        CameraX.bindToLifecycle((LifecycleOwner)this, analysis, imgCap, preview);
+        CameraX.bindToLifecycle((LifecycleOwner) this, analysis, imgCap, preview);
     }
 
-    private void updateTransform(){
+    public interface UploadAPIs {
+        @Multipart
+        @POST("mobile")
+        Call<ResponseBody> uploadImage(@Header("accept") String type, @Part MultipartBody.Part file1);
+    }
+
+    public static class NetworkClient {
+        private static SharedPreferences prefs;
+        String name = prefs.getString("", "");
+        private static String BASE_URL = "";
+        private static Retrofit retrofit;
+
+        public static Retrofit getRetrofitClient(Context context) {
+            prefs = PreferenceManager.getDefaultSharedPreferences(context);
+            BASE_URL = prefs.getString("signature", "http://127.0.0.1:5000/");
+
+            if (BASE_URL.isEmpty()) {
+
+                Toast.makeText(context, "Please set the http URL in settings page", Toast.LENGTH_LONG).show();
+            }
+
+            if (retrofit == null) {
+                HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+// set your desired log level
+                logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+                OkHttpClient.Builder okHttpClient = new OkHttpClient.Builder();
+                okHttpClient.readTimeout(1000, TimeUnit.SECONDS);
+                okHttpClient.connectTimeout(500, TimeUnit.SECONDS);
+                okHttpClient.addInterceptor(logging);
+                okHttpClient.retryOnConnectionFailure(true);
+
+                retrofit = new Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .client(okHttpClient.build())
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+            }
+            return retrofit;
+        }
+
+    }
+
+    private void uploadToServer(File file) {
+        final ProgressDialog progressDialog;
+        progressDialog = new ProgressDialog(MainActivity.this);
+        //progressDialog.setMax(100);
+        progressDialog.setMessage("Calculating results");
+        progressDialog.setTitle("Please wait...");
+        //progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setCancelable(false);
+        // show it
+        progressDialog.show();
+
+        Retrofit retrofit = NetworkClient.getRetrofitClient(this);
+        UploadAPIs uploadAPIs = retrofit.create(UploadAPIs.class);
+
+        RequestBody fileReqBody1 = RequestBody.create(MediaType.parse("image/*"), file);
+
+        // Create MultipartBody.Part using file request-body,file name and part name
+        MultipartBody.Part part1 = MultipartBody.Part.createFormData("image", file.getName(), fileReqBody1);
+
+
+        Call<ResponseBody> call = uploadAPIs.uploadImage("application/json", part1);
+        call.enqueue(new Callback<ResponseBody>() {
+
+            @Override
+            public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> response) {
+
+                if (response.isSuccessful()) {
+                    Log.e(TAG, response.body().toString());
+                    String prediction = "";
+                    String json = response.message().toString();
+                    try {
+                        JSONObject jsonObj = new JSONObject(json);
+                        prediction = jsonObj.get("response").toString();
+                    } catch (JSONException e) {
+                        prediction = "Something went wrong! Please try again";
+                    }
+                    progressDialog.dismiss();
+                    Toast.makeText(getBaseContext(), prediction, Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                progressDialog.dismiss();
+                t.printStackTrace();
+                Log.e(TAG, t.getMessage());
+            }
+        });
+    }
+
+
+    private void updateTransform() {
         /*
          * compensates the changes in orientation for the viewfinder, bc the rest of the layout stays in portrait mode.
          * methinks :thonk:
@@ -141,9 +269,9 @@ public class MainActivity extends AppCompatActivity {
         float centreY = h / 2f;
 
         int rotationDgr;
-        int rotation = (int)txView.getRotation(); //cast to int bc switches don't like floats
+        int rotation = (int) txView.getRotation(); //cast to int bc switches don't like floats
 
-        switch(rotation){ //correct output to account for display rotation
+        switch (rotation) { //correct output to account for display rotation
             case Surface.ROTATION_0:
                 rotationDgr = 0;
                 break;
@@ -160,27 +288,54 @@ public class MainActivity extends AppCompatActivity {
                 return;
         }
 
-        mx.postRotate((float)rotationDgr, centreX, centreY);
+        mx.postRotate((float) rotationDgr, centreX, centreY);
         txView.setTransform(mx); //apply transformations to textureview
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+
+            Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(settingsIntent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         //start camera when permissions have been granted otherwise exit app
-        if(requestCode == REQUEST_CODE_PERMISSIONS){
-            if(allPermissionsGranted()){
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
                 startCamera();
-            } else{
+            } else {
                 Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
                 finish();
             }
         }
     }
 
-    private boolean allPermissionsGranted(){
+    private boolean allPermissionsGranted() {
         //check if req permissions have been granted
-        for(String permission : REQUIRED_PERMISSIONS){
-            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
                 return false;
             }
         }
